29eec3e3125ab76d35887cef37deb60566c3fd43
Marc Jacob <marcjacobpro@gmail.com>
2018-04-06 15:18:23 +0200

    Gestion basique des vidéos.

--- js/media.js
+++ js/media.js
@@ -1,5 +1,87 @@
 // Ensemble de fonctions utilitaires
 // de manipulation de la section "media".
 
-// Sert également à initialiser la section
+// La section media va lire le fichier actuel s'il y en a un, sinon il passe au
+// prochain dans la playlist.
 
+// Si la playlist est vide, alors elle attend qu'un élément soit ajouté.
+
+// Interface
+
+var mediaControls = document.getElementById("controls");
+var videoScreen = document.getElementById("media").children.item(0);
+var audioImage = document.getElementById("media").children.item(1);
+var currentElement = null; // Element (vidéo ou audio) entrain d'être lu.
+//
+
+// Event listeners
+
+document.addEventListener("PlaylistElementCreated", onPlaylistElementAdded);
+
+mediaControls.children.item(0).addEventListener("click", onPause);
+
+videoScreen.addEventListener("ended", onCurrentElementEnded);
+
+//
+
+// Event handlers
+
+function onPlaylistElementAdded(event)
+{
+    if (currentElement === null)
+        {
+            console.log("No source");
+            setElementAsSource(event.element);
+        }
+}
+
+function setElementAsSource(element)
+{
+    // get element type
+    var splitString = element.children.item(3).value.split(".");
+    var extension = splitString[splitString.length-1];
+        videoScreen.children.item(0).src = element.children.item(3).value;
+    currentElement = videoScreen.children.item(0).src;
+        videoScreen.children.item(0).type = "video/mp4";
+    videoScreen.load();
+    videoScreen.play();
+}
+
+function onCurrentElementEnded(element)
+{
+    currentElement = null;
+    // On enlève le premier élément de la playlist.
+    list.removeChild(list.children.item(0));
+}
+
+function onPause(e)
+{
+    if (videoScreen.paused)
+        {
+            mediaControls.children.item(0).textContent = "Pause";
+            videoScreen.play();
+        }
+    else
+        {
+            mediaControls.children.item(0).textContent = "Resume";
+            videoScreen.pause();
+        }
+}
+
+function onSkip(e)
+{
+    onCurrentElementEnded(e);
+}
+
+// ______________________________________
+
+// Barre de progression
+progressUpdate();
+function progressUpdate()
+{
+    if (currentElement !== null && videoScreen.duration !== undefined && videoScreen.duration !== null && videoScreen.duration !== 0)
+        {
+    mediaControls.children.item(2).value = videoScreen.currentTime / videoScreen.duration * 100;
+        }
+    setTimeout(progressUpdate, 500);
+}
--- js/playlist.js
+++ js/playlist.js
@@ -9,17 +9,95 @@ var list = document.getElementById("list");
 
 //
 
-// Création d'un élément de la liste
+// Création d'un élément de la liste 
 
 var listElement = document.createElement("div");
 listElement.className = "playlistElement";
 listElement.appendChild(document.createElement("button")); // Création bouton supprimer.
-listElement.appendChild(document.createElement("button")); // Création bouton augmenter priorité.
+listElement.appendChild(document.createElement("button")); // Création bouton augmenter priorité.gt 'èg
 listElement.appendChild(document.createElement("button")); // Création bouton baisser priorité.
 listElement.appendChild(document.createElement("textarea"));
 
 listElement.children.item(0).textContent = "x";
 listElement.children.item(1).textContent = "-";
 listElement.children.item(2).textContent = "+";
+// Sera cloné à l'ajout d'un élément à la playlist.
 
-list.appendChild(listElement);
+
+
+// FONCTIONS RELATIVES AUX ELEMENTS DE PLAYLIST
+
+// Fonctions de manipulation des éléments de la playlist
+
+function onPlaylistElementDelete(element)
+{
+    list.removeChild(element);
+    // TODO : Vérifier que l'élément ne soit pas en cours
+    // de lecture. Si c'est le cas, interrompre la lecture
+    // et passer au prochain élément.
+}
+
+function onPlaylistElementPriorityUp(element)
+{
+    if (list.children.length > element.priority+1)
+        {
+            console.log(element.priority);
+            list.insertBefore(list.children.item(element.priority+1), element);
+            list.children.item(element.priority).priority--;
+            element.priority++;
+        }
+}
+
+function onPlaylistElementPriorityDown(element)
+{
+    if (element.priority > 0)
+        {
+            list.insertBefore(element, list.children.item(element.priority-1));
+            list.children.item(element.priority).priority++;
+            element.priority--;
+        }
+}
+
+// Renvoi un nouvel élément de playlist contenant le lien vers l'élément ajouté. L'ajoute à l'élément "list".
+function createListElement(link)
+{
+    var newElement = listElement.cloneNode(true);
+    newElement.children.item(3).textContent = link;
+    newElement.children.item(3).readOnly = true;
+    newElement.children.item(0).addEventListener("click", function() {onPlaylistElementDelete(newElement);});
+    newElement.children.item(1).addEventListener("click", function() {onPlaylistElementPriorityUp(newElement);});
+    newElement.children.item(2).addEventListener("click", function() {onPlaylistElementPriorityDown(newElement);});
+    newElement.priority = list.children.length;
+    console.log("Created element with priority " + newElement.priority);
+    list.appendChild(newElement);
+    var playlistElementCreatedEvent = new Event("PlaylistElementCreated");
+    playlistElementCreatedEvent.element = newElement;
+    document.dispatchEvent(playlistElementCreatedEvent);
+    return newElement;
+}
+
+
+
+
+//____________________________________________________
+
+// Vérifie si du texte est présent dans le textArea contenant le lien.
+// Si du texte se trouve dedans, alors un nouvel élément est ajouté à la playlist avec le lien indiqué dans le textArea. Le contenu du textArea est vidé.
+function onPlaylistAddButtonClicked()
+{
+    var link = controls.children.item(1).value;
+    if (link === null || list === undefined || link.length > 0)
+        {
+            createListElement(link);
+            controls.children.item(1).value = "";
+        }
+    else
+        {
+            console.log("ERREUR : Pas de liens !");
+        }
+}
+
+controls.children.item(0).addEventListener("click", onPlaylistAddButtonClicked);
+
+
+
